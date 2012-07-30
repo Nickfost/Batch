553288222d5046d2102f205411c22e7dbc8683f5
Nick Foster <Me@nickfost.com>
2012-07-30 18:12:25 -0500

    Updated some junk

--- Calc.bat
+++ Calc.bat
@@ -1,33 +1,35 @@
-@echo off
-title Calc
-echo Please enter a number with the corrisponding type of math
-Echo 1. Addition
-echo 2. Subtraction
-Echo 3. Multiplication
-echo 4. Division
+@ECHO off
+title Nickfost's Calculator
+ECHO -----------------------------------------------------------
+ECHO ^|Please enter a number with the corrisponding type of math^|
+ECHO ----------------^|1. Addition       ^|-----------------------
+ECHO                 ^|2. Subtraction    ^|
+ECHO                 ^|3. Multiplication ^|
+ECHO                 ^|4. Division       ^|
+ECHO                  -------------------
 :start
 set/p menu=
 if %menu%==1 goto add
 if %menu%==2 goto sub
 if %menu%==3 goto mul
 if %menu%==4 goto div
-echo  We are sorry that was not a valid response.
-echo  Please try agian
+ECHO sorry that was not a valid response.
+ECHO Please try agian
 goto start
 :q
 cls
-echo Please provide the first number %to% %mathtype3% %from%
+ECHO Please provide the first number %to% %mathtype3% %from%
 set/p first=
 cls
-echo Please provide the second number to %mathtype4%
-echo Your current problem stands at %first% %mathtype2% ? = ?
+ECHO Please provide the second number to %mathtype4%
+ECHO Your current problem stands at %first% %mathtype2% ? = ?
 set/p second=
 cls
 set /a math= %first% %mathtype2% %second% 
-echo Your result is:
-echo %first% %mathtype2% %second% = %math%
-echo Would you like to do another math problem?
-echo y/n
+ECHO Your result is:
+ECHO %first% %mathtype2% %second% = %math%
+ECHO Would you like to do another math problem?
+ECHO y/n
 set/p yehno=
 if "%yehno%"=="n" exit
 if "%yehno%"=="y" Start Calc.bat
@@ -54,5 +56,4 @@ goto q
 set mathtype2=/
 set mathtype4= Divide by
 goto q
-echo Nickfost made this :D.... 
-echo seriously, he did!
+ECHO Nickfost made this :D....

5b3bb5287d54041102f534d04a5acb9055f541da
Nick Foster <me@nickfost.com>
2016-01-09 07:52:54 -0600

    Pranks 2016

---
+++ Prank Virus/Prank2016.bat
@@ -0,0 +1,284 @@
+@echo OFF
+TITLE VIRUS ALERT
+echo ##     ## #### ########  ##     ##  ######  
+echo ##     ##  ##  ##     ## ##     ## ##    ## 
+echo ##     ##  ##  ##     ## ##     ## ##       
+echo ##     ##  ##  ########  ##     ##  ######  
+echo  ##   ##   ##  ##   ##   ##     ##       ## 
+echo   ## ##    ##  ##    ##  ##     ## ##    ## 
+echo    ###    #### ##     ##  #######   ###### 
+echo.
+echo    ###    ##       ######## ########  ######## 
+echo   ## ##   ##       ##       ##     ##    ##    
+echo  ##   ##  ##       ##       ##     ##    ##    
+echo ##     ## ##       ######   ########     ##    
+echo ######### ##       ##       ##   ##      ##    
+echo ##     ## ##       ##       ##    ##     ##    
+echo ##     ## ######## ######## ##     ##    ##   
+ping 1.1.1.1  -w 100 >NUL
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+title Gathering data
+set ping=0
+:ping
+PING www.trojan-virus.com -n 1
+SET /a ping=%ping% + 1
+if /i "%ping%"=="10" goto alert
+GOTO ping
+:alert
+ping 1.1.1.1 -w 100 >NUL
+cls
+title ALERT
+cd c:/
+echo @echo off > pop.bat
+echo title YOU'RE FUCKED >> pop.bat
+echo echo YOU'RE FUCKED >> pop.bat
+echo ping 1.1.1.1 ^>NUL >>pop.bat
+echo exit >>pop.bat
+echo VIRUS HAS PROGRESSED TO C: DRIVE
+ping 1.1.1.1 -w 33 >NUL
+echo.
+echo ALL FILES IN C: WILL BE DELETED
+ping 1.1.1.1 -w 100 >NUL
+echo.
+echo ALL FILES IN C: DRIVE ARE BEING DELETED
+ping 1.1.1.1 -w 1000 >NUL
+echo.
+ping 1.1.1.1 -w 500 >NUL
+echo.
+ping 1.1.1.1 -w 500 >NUL
+echo.
+ping 1.1.1.1 -w 500 >NUL
+echo.
+ping 1.1.1.1 -w 1000 >NUL
+echo PANIC
+ping 1.1.1.1 -n 1 -w 30 >NUl
+tree
+cls
+echo ALL FILES IN C: HAVE BEEN DELETED
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ALL FILES IN C: HAVE BEEN DELETED.
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ALL FILES IN C: HAVE BEEN DELETED..
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ALL FILES IN C: HAVE BEEN DELETED...
+ping 1.1.1.1 -w 100 >NUL
+cls
+echo VIRUS STILL DETECTED
+ping 1.1.1.1 -n 1 -w 1 >NUL
+echo      ERROR
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo VIRUS STILL DETECTED
+echo.
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo VIRUS STILL DETECTED
+echo      ERROR
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo VIRUS STILL DETECTED
+echo.
+echo.
+echo.
+ping 1.1.1.1 -n -w 10 >NUL
+cls
+echo VIRUS STILL DETECTED
+echo      ERROR
+ping 1.1.1.1 -w 425 >NUL
+cls
+echo ATTEMPTING REPAIRS
+ping 1.1.1.1 -n 1 -w 100 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo /
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo \
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo /
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo \
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo /
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo \
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo /
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo \
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo /
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo \
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo /
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo \
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo /
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo \
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo /
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo \
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo /
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo \
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo /
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo \
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo /
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo \
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo /
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo \
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo /
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo \
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo /
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo \
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo /
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo \
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo /
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo \
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo /
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo \
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo /
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo \
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo /
+ping 1.1.1.1 -n 1 -w 1 >NUL
+cls
+echo ATTEMPTING REPAIRS
+echo     FAILED
+ping 1.1.1.1 -n 1 -w 1000 >NUL
+:pops
+set pop=0
+:pop
+cls
+ECHO YOU'RE FUCKED
+if /i "%pop%"=="1000" goto idk
+SET /a pop=%pop% + 1
+start pop.bat
+goto pop
+:idk
+ping 1.1.1.1 -n 10 -w 3000 >NUL
+start iexplore -private -k http://nckfst.us/win8plusbsod >NUL
+ping 1.1.1.1 -n 10 -w 3000 >NUL
+start iexplore  -k http://nckfst.us/hey 
+ping 1.1.1.1 -n 10 -w 3000 >NUL
+del pop.bat
+exit
+REM ================
+REM ||NICKFOST.NET||
+REM ================
